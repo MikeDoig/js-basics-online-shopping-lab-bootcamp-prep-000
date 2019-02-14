0146ed1eff3c4420dbea8164281cceec37aa231e
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 23:49:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ getCart().push(thing);
 function fullCart() {
   var thingsInCart = 'In your cart, you have ';
   if (getCart().length === 1) {
-    return thingsInCart + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`;
+    thingsInCart = thingsInCart + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`;
   }
   else if (getCart().length > 1) {
     var otherThingsInCart = ' ';
     for (let i=1; i < getCart().length - 1; i++) {
-    otherThingsInCart + `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`;
+    otherThingsInCart = otherThingsInCart + `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`;
   }
-   return thingsInCart + `${otherThingsInCart}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}.`;
+    return thingsInCart + `${otherThingsInCart}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}.`;
   }
 }
 
