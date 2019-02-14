c77e9d856dc236fc3d44fa7e2e7fd7729a48e2b8
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 23:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function fullCart() {
     for (let i=1; i < getCart().length - 1; i++) {
     otherThingsInCart = otherThingsInCart + `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`;
   }
-    return thingsInCart + `${otherThingsInCart}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}.`;
+    thingsInCart = thingsInCart + `${otherThingsInCart}, and ${getCart()[getCart().length - 1].itemName} at $${getCart()[getCart().length - 1].itemPrice}.`;
   }
 }
 
