967a86375b60fd1deb8b99a165963a8339f642e0
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 22:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-var cart = [];
+// var cart = [];
 
-function getCart() {
- return cart;
-}
+// function getCart() {
+// return cart;
+// }
 
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-function createCartItem(item) {
-  return {item:itemName, itemPrice:Math.random(1,100)};
-}
+// function setCart(c) {
+//   cart = c;
+//   return cart;
+// }
+// function createCartItem(item) {
+//   return {item:itemName, itemPrice:Math.random(1,100)};
+// }
 
-function addToCart(item) {
+// function addToCart(item) {
   
-var item = createCartItem(item);
-getCart().push(item);
-  return `${item.itemName} has been added to your cart.`;
-}
+// var item = createCartItem(item);
+// getCart().push(item);
+//   return `${item.itemName} has been added to your cart.`;
+// }
 
-function viewCart() {
-  if (cart.length === 0) {
-    return 'Your shopping cart is empty.';
-  }
-  else {
-    const contents = [];
-    for (let i=0, l=cart.length; i<l; i++){
-    contents.push(`${i + 1}. ${cart[i]}`);
-  }
-  return `In your cart, you have ${contents.join('at $')}.`;
-  }
-}
+// function viewCart() {
+//   if (cart.length === 0) {
+//     return 'Your shopping cart is empty.';
+//   }
+//   else {
+//     const contents = [];
+//     for (let i=0, l=cart.length; i<l; i++){
+//     contents.push(`${i + 1}. ${cart[i]}`);
+//   }
+//   return `In your cart, you have ${contents.join('at $')}.`;
+//   }
+// }
 
-function total() {
+// function total() {
 
-}
+// }
 
-function removeFromCart(item) {
+// function removeFromCart(item) {
 
-}
+// }
 
-function placeOrder(cardNumber) {
+// function placeOrder(cardNumber) {
 
-}
+// }
 
-function createCartItem(item) {
-  return {item:itemName, itemPrice:Math.random(1,100)};
-}
+// function createCartItem(item) {
+//   return {item:itemName, itemPrice:Math.random(1,100)};
+// }
