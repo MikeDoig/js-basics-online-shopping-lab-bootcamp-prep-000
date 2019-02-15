a7d52d24d3a8b5c57c3effae2d26af476ff03f9e
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 17:12:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,24 +76,24 @@ function removeFromCart(item) {
 
 
 
-// function removeFromCart(itemName) {
-//   function searchCartForItemToRemove(itemName) {
-//   var searchResult
-//   for (var i=0; i<getCart().length; i++) {
-//     if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
-//   }
-//   return searchResult
-// }
-// function removeItemFromCart(itemToRemove) {
-//   var indexOfItemToRemove = cart.indexOf(itemToRemove)
-//   getCart().splice(indexOfItemToRemove,1)
-// }
-//   var itemToRemove = searchCartForItemToRemove(itemName)
-//   return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
-// }
-// function notifyUserThereIsNoItemToRemove() {
-//   return 'That item is not in your cart.'
-// }
+function removeFromCart(itemName) {
+  function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i<getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  getCart().splice(indexOfItemToRemove,1)
+}
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
+}
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
+}
 
 
 function placeOrder(cardNumber) {
