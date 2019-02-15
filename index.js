3032c363156b282cccfa8003031190fc4eba7af8
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 00:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function addingPrices() {
   for (let i=0; i < getCart().length; i++) {
     sum = sum + `${getCart()[i].itemPrice}`;
   }
+  return sum;
 }
 function total() {
 return addingPrices();
