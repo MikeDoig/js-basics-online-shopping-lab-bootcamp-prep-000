f9a851af51c69002276feb35854e92ef93cf11b4
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 17:25:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,8 +86,10 @@ function removeItemFromCart(itemToRemove) {
   var indexOfItemToRemove = cart.indexOf(itemToRemove)
   getCart().splice(indexOfItemToRemove,1)
 }
-
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
 }
+
 
 
 
